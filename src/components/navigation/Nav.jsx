5517bf3043e0377d7db477fb8158969bf1c13a1e
joker-jonesy <spashmcgrath94@yahoo.com>
2024-05-20 12:50:05 -0400

    kermit

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,14 +1,28 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFish} from "@fortawesome/free-solid-svg-icons/faFish";
+import {faBars} from "@fortawesome/free-solid-svg-icons/faBars";
 
 function Nav(){
+
+    const [toggle, setToggle]=useState(false);
+
     return(
         <>
+            <div className={"mobile_menu"} style={{left: toggle? "0": "-100%"}}>
+                <Link to={"/"} onClick={()=>setToggle(!toggle)}>All Fishes</Link>
+            </div>
             <nav>
-                <div className="logo"></div>
-                <div className="link-container">
+                <Link to={"/"} className="logo">
+                    <FontAwesomeIcon icon={faFish} size={"3x"}/>
+                </Link>
+                <div className="link-container" >
                     <Link to={"/"}>All Fishes</Link>
                 </div>
+                <div className={"menu_toggle"} onClick={()=>setToggle(!toggle)}><FontAwesomeIcon icon={faBars} size={"2x"}/></div>
             </nav>
+
         </>
 
     )
